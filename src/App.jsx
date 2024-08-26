d0d0228e350c7a4f88c1ea50ad6115b3f3463777
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-08-26 19:29:27 +0100

    Initial Commits

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css'
+import UseContextBasics from './useContext Hook/useContext-basics.jsx'
+
+export default function App() {
+  return (
+    <>
+      <UseContextBasics />
+    </>
+  )
+}
